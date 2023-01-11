c33f9f00d420d2ccde56358233d05aadb5826716
Igor de Moraes <r.de.moraes.igor@gmail.com>
2023-01-11 07:52:52 -0300

    Changes for deploy

--- src/db.js
+++ src/db.js
@@ -1,13 +1,11 @@
 import pkg from "pg";
-
+import dotenv from "dotenv";
+dotenv.config();
 const { Pool } = pkg;
 
 const connection = new Pool({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: "29062007",
-  database: "la_boleria",
+  connectionString: process.env.DATABASE_URL,
+  //ssl: true,
 });
 
 export default connection;
